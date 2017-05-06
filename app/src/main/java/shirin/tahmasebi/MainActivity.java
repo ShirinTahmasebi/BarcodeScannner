2b76d8dd0760d5fbce19b4dfd74166182be15613
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2017-05-06 05:23:49 +0430

    پکیجها رو ربفکتور کن - عملیات اسکن را آغاز کن

---
+++ app/src/main/java/shirin/tahmasebi/MainActivity.java
@@ -0,0 +1,14 @@
+package shirin.tahmasebi;
+
+import android.os.Bundle;
+
+import com.google.zxing.integration.android.*;
+
+import tahmasebi.shirin.barcodescanner.R;
+
+public class MainActivity implements  {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/test/java/shirin/tahmasebi/MainActivity.java
+++
@@ -1,4 +0,0 @@
-package shirin.tahmasebi;
-
-public class MainActivity {
-}
