4930ee652cfc7e9b670ff71e9182f7ab07ea733a
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2017-05-06 05:37:59 +0430

    نتیجه اسکن را بگیر و نمایش بده

--- app/src/main/java/shirin/tahmasebi/MainActivity.java
+++ app/src/main/java/shirin/tahmasebi/MainActivity.java
@@ -1,14 +1,62 @@
 package shirin.tahmasebi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.zxing.integration.android.*;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import tahmasebi.shirin.barcodescanner.R;
 
-public class MainActivity implements  {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button scan_button;
+    private TextView format_textview, content_textview;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        scan_button = (Button) findViewById(R.id.scan_button);
+        format_textview = (TextView) findViewById(R.id.scan_format);
+        content_textview = (TextView) findViewById(R.id.scan_content);
+
+        scan_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.scan_button) {
+            // Scan
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        // Retrieve scan result
+        IntentResult scanningResult =
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+            // We have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            format_textview.setText("FORMAT: " + scanFormat);
+            content_textview.setText("CONTENT: " + scanContent);
+        } else {
+            Toast toast =
+                    Toast.makeText(
+                            getApplicationContext(),
+                            R.string.error_nullScanResult,
+                            Toast.LENGTH_SHORT
+                    );
+            toast.show();
+        }
     }
 }
